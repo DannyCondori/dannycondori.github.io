4f421010138b4df3c6064845fef3af6945675589
DannyCondori <126608704+DannyCondori@users.noreply.github.com>
2023-03-25 01:55:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
 
 const  jamón  =  documento . querySelector ( '.jamón' ) ;
 const  enlaces  =  documento . querySelector ( '.enlaces-menu' ) ;
